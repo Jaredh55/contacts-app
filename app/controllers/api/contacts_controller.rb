00e9f081c06d1087b5e673767ffe64848f0c2679
Jaredh55 <jaredh55@gmail.com>
2018-05-10 21:32:15 -0500

    restful

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,11 +1,56 @@
 class Api::ContactsController < ApplicationController
-  def first_contact_action
-    @contact = Contact.find(1)
-    render 'first_contact_view.json.jbuilder'
+  def index
+    @contacts = Contact.all
+    render 'index.json.jbuilder'
   end
 
-  def allcontacts
-    @contacts = Contact.all
-    render 'allcontactsview.json.jbuilder'
+  def show
+    contact_id = params[:id]
+    @contact = Contact.find(contact_id)
+    render 'show.json.jbuilder'
   end
+
+  def create
+    @contact = Contact.new(
+                        
+                            first_name: params[:first_name]
+                            last_name: params[:last_name]
+                            email: params[:email]
+                            phone: params[:phone]
+                            )
+    @contact.save
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    contact_id = params[:id]
+    @contact = Contact.find(contact_id)
+
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone = params[:phone] || @contact.phone
+
+    @contact.save
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    contact_id = params[:id]
+    @contact = Contact.find(contact_id)
+    @contact.destroy
+    render json: {message: "Contact removed."}
+  end
+
+
+
+  # def first_contact_action
+  #   @contact = Contact.find(1)
+  #   render 'first_contact_view.json.jbuilder'
+  # end
+
+  # def allcontacts
+  #   @contacts = Contact.all
+  #   render 'allcontactsview.json.jbuilder'
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
-    get '/first_contact_url' => 'contacts#first_contact_action'
-    get 'all_contacts' => 'contacts#allcontacts'
+    get '/contacts' => 'contacts#index'
+    get '/contacts/:id' => 'contacts#show'
+    post '/contacts' => 'contacts#create'
+    patch '/contacts/:id' => 'contacts#update'
+    delete '/contacts/:id' => 'contacts#destroy'
+    # get '/first_contact_url' => 'contacts#first_contact_action'
+    # get 'all_contacts' => 'contacts#allcontacts'
   end
 end
